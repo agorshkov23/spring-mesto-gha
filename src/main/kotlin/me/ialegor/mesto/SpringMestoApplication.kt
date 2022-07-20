a44da536208d03b1af8ebdc3e27936121950c82d
Alexey Gorshkov <agorshkov23@mail.ru>
2022-07-20 22:42:04 +0300

    generated project from https://start.spring.io/

---
+++ src/main/kotlin/me/ialegor/mesto/SpringMestoApplication.kt
@@ -0,0 +1,11 @@
+package me.ialegor.mesto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMestoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringMestoApplication>(*args)
+}
---
+++ src/test/kotlin/me/ialegor/mesto/SpringMestoApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.ialegor.mesto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMestoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
